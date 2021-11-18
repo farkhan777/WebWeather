983b5fe4f9dee625ca54a10978c1abd387f541c0
farkhan <farhanhamzah71@webmail.umm.ac.id>
2021-11-18 12:57:28 +0700

    arrow funcntion

--- src/index.js
+++ src/index.js
@@ -11,16 +11,25 @@ const desc = document.querySelector('.desc');
 const button= document.querySelector('.submitBtn');
 
 
-button.addEventListener('click', function(e){
+button.addEventListener('click', (e) => {
     fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=metric&appid=7b6dca8b1390af80b8db39b6627e5652')
     .then(response => response.json())
     .then(data => {
-        main.innerHTML = data['name'] + ', ' + data['sys']['country'];
-        wind.innerHTML = "WIND &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+data['wind']['speed']+" m/s";
-        pressure.innerHTML = "PRESSURE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+data['main']['pressure']+" hPa";
-        humidity.innerHTML = "HUMIDITY &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+data['main']['humidity']+" %";
-        desc.innerHTML = "DESCRIPTION &nbsp;&nbsp;&nbsp;&nbsp;"+data['weather'][0]['description'];
-        temp.innerHTML = Math.round(data['main']['temp']) + ' &deg;C';
+
+        const valName = data['name'];
+        const valVountry = data['sys']['country'];
+        const valWindSpeed = data['wind']['speed'];
+        const valPressure = data['main']['pressure'];
+        const valHumidity = data['main']['humidity'];
+        const valDesc = data['weather'][0]['description'];
+        const valTemp = data['main']['temp'];
+
+        main.innerHTML = `${valName}, ${valVountry}`;
+        wind.innerHTML = `WIND &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${valWindSpeed} m/s`;
+        pressure.innerHTML = `PRESSURE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${valPressure} hPa`;
+        humidity.innerHTML = `HUMIDITY &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${valHumidity} %`;
+        desc.innerHTML = `DESCRIPTION &nbsp;&nbsp;&nbsp;&nbsp; ${valDesc}`;
+        temp.innerHTML = `${Math.round(valTemp)} &deg;C`;
         input.value ="";
 
     })
